1fb69fbbf9466bb7942f6ac3837ae3b5372dbc91
Rutvik <rrutvikpandya00@gmail.com>
2021-07-15 08:54:08 +0530

    Client side libraries added

--- RuTTiK27.BookStore/RuTTiK27.BookStore/Startup.cs
+++ RuTTiK27.BookStore/RuTTiK27.BookStore/Startup.cs
@@ -16,6 +16,8 @@ namespace RuTTiK27.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +27,14 @@ namespace RuTTiK27.BookStore
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
+
         }
     }
 }
